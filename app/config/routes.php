65383241d80fd2e95210e8bc621f7f0ab4f89e3d
Nguyen Bao Ngoc <ngocnguyenbao2707@gmail.com>
2024-03-23 12:05:16 +0700

    #6 fix route of register api

--- app/config/routes.php
+++ app/config/routes.php
@@ -29,7 +29,7 @@ use Cake\Routing\Route\DashedRoute;
  * So you can use  `$this` to reference the application class instance
  * if required.
  */
-return function (RouteBuilder $routes): void{
+return function (RouteBuilder $routes): void {
     /*
      * The default class to use for all routes
      *
@@ -51,7 +51,7 @@ return function (RouteBuilder $routes): void{
 
     $routes->setExtensions(['json', 'xml']);
 
-    $routes->scope('/', function (RouteBuilder $builder): void{
+    $routes->scope('/', function (RouteBuilder $builder): void {
         /*
          * Here, we are connecting '/' (base path) to a controller called 'Pages',
          * its action called 'display', and we pass a param to select the view file
@@ -76,23 +76,24 @@ return function (RouteBuilder $routes): void{
     });
 
     /*
- * If you need a different set of middleware or none at all,
- * open new scope and define routes there.
- *
- * ```
- * $routes->scope('/api', function (RouteBuilder $builder): void {
- *     // No $builder->applyMiddleware() here.
- *
- *     // Parse specified extensions from URLs
- *     // $builder->setExtensions(['json', 'xml']);
- *
- *     // Connect API actions here.
- * });
- * ```
- */
+     * If you need a different set of middleware or none at all,
+     * open new scope and define routes there.
+     *
+     * ```
+     * $routes->scope('/api', function (RouteBuilder $builder): void {
+     *     // No $builder->applyMiddleware() here.
+     *
+     *     // Parse specified extensions from URLs
+     *     // $builder->setExtensions(['json', 'xml']);
+     *
+     *     // Connect API actions here.
+     * });
+     * ```
+     */
     $routes->scope('/api', function (RouteBuilder $builder): void {
         $builder->setExtensions(['json']);
-    });
 
+        $builder->connect('/users/register', ['controller' => 'Users', 'action' => 'register', '_method' => 'POST']);
+    });
 
 };
